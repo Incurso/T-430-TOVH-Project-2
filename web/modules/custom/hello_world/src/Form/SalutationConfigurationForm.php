39969881f106b5de81d8327c30494b63ad7c3d46
Einar Sigurðsson <einarsig16@ru.is>
2021-05-11 15:23:48 +0000

    basic template for salutationconfigurationform

--- web/modules/custom/hello_world/src/Form/SalutationConfigurationForm.php
+++ web/modules/custom/hello_world/src/Form/SalutationConfigurationForm.php
@@ -4,6 +4,23 @@ namespace Drupal\hello_world\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 
+/**
+ * Configuration form definition for the salutation message
+ */
 class SalutationConfigurationForm extends ConfigFormBase {
-  
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames()
+  {
+    // TODO: Implement getEditableConfigNames() method.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId()
+  {
+    // TODO: Implement getFormId() method.
+  }
 }
