877da4709945c7b99e10dbe399b25684ff764702
Einar Sigurðsson <einarsig16@ru.is>
2021-05-12 22:20:12 +0000

    WIP: authentication against spotify with basic search

--- web/modules/custom/music_search/src/Form/MusicSearchConfigurationForm.php
+++ web/modules/custom/music_search/src/Form/MusicSearchConfigurationForm.php
@@ -31,18 +31,18 @@ class MusicSearchConfigurationForm extends ConfigFormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $config = $this->config('music_search.settings');
 
-    $form['client_id'] = [
+    $form['spotify_client_id'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Client ID'),
+      '#title' => $this->t('Spotify Client ID'),
       '#description' => $this->t('Please provide the Spotify Client ID'),
-      '#default_value' => $config->get('client_id')
+      '#default_value' => $config->get('spotify_client_id')
     ];
 
-    $form['client_secret'] = [
+    $form['spotify_client_secret'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('Client Secret'),
+      '#title' => $this->t('Spotify Client Secret'),
       '#description' => $this->t('Please provide the Spotify Client Secret'),
-      '#default_value' => $config->get('client_secret')
+      '#default_value' => $config->get('spotify_client_secret')
     ];
 
     return parent::buildForm($form, $form_state);
@@ -52,9 +52,9 @@ class MusicSearchConfigurationForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    $client_id = $form_state->getValue('client_id');
+    $client_id = $form_state->getValue('spotify_client_id');
     if(strlen($client_id) > 32) {
-      $form_state->setErrorByName('client_id', $this->t('The Client ID is to long'));
+      $form_state->setErrorByName('spotify_client_id', $this->t('The Spotify Client ID is to long'));
     }
 
     parent::validateForm($form, $form_state);
@@ -65,8 +65,8 @@ class MusicSearchConfigurationForm extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $this->config('music_search.settings')
-      ->set('client_id', $form_state->getValue('client_id'))
-      ->set('client_secret', $form_state->getValue('client_secret'))
+      ->set('spotify_client_id', $form_state->getValue('spotify_client_id'))
+      ->set('spotify_client_secret', $form_state->getValue('spotify_client_secret'))
       ->save();
 
     parent::submitForm($form, $form_state);
--- web/modules/custom/music_search/src/MusicSearchService.php
+++ web/modules/custom/music_search/src/MusicSearchService.php
@@ -39,18 +39,112 @@ class MusicSearchService {
   }
 
   /**
+   * Returns access token
+   */
+  private function login() {
+    $request = \Drupal::request();
+    $session = $request->getSession();
+    $access_token = $session->get('spotify_access_token');
+    $config = $this->configFactory->get('music_search.settings');
+    $client_id = $config->get('spotify_client_id');
+    $client_secret = $config->get('spotify_client_secret');
+    $date = new \DateTime();
+
+    $options = array(
+      'headers' => array(
+        'Accept' => 'application/json',
+        'Authorization' => 'Basic '. \base64_encode($client_id.':'.$client_secret),
+        'Content-Type' => 'application/x-www-form-urlencoded',
+      ),
+      'form_params' => array(
+        'grant_type' => 'client_credentials'
+      )
+    );
+
+    $uri = 'https://accounts.spotify.com/api/token';
+
+    # TODO: Attempt login
+    # Authenticate against Spotify
+    $response = \Drupal::httpClient()->post($uri, $options);
+
+    # Assume we get access token as a response
+    $access_token = \json_decode($response->getBody(), true);
+    # Add issued_at to simplify expiration checks
+    $access_token['issued_at'] = $date->getTimestamp();
+    # Store access token in session
+    $session->set('spotify_access_token', $access_token);
+  }
+
+  /**
+   * Returns boolean if token is expired or not
+   */
+  private function token_expired() {
+    $request = \Drupal::request();
+    $session = $request->getSession();
+    $access_token = $session->get('spotify_access_token');
+    $date = new \DateTime();
+
+    return (
+      $access_token == "" &&
+      !$access_token ||
+      ($access_token['issued_at'] + $access_token['expires_in']) < $date->getTimestamp()
+    );
+  }
+
+  /**
    * Returns the salutation.
    *
    * @return \Drupal\Core\StringTranslation\TranslatableMarkup
    *   The salutation message.
    */
   public function getSalutation() {
-    $config = $this->configFactory->get('music_search.settings');
-    $client_id = $config->get('client_id');
+    $request = \Drupal::request();
+    $session = $request->getSession();
+    $access_token = $session->get('spotify_access_token');
+    #$session->set('spotify_access_token', '');
+
+    if ($this->token_expired()) {
+      $this->login();
+      $access_token = $session->get('spotify_access_token');
+    }
+
+    $options = array(
+      'headers' => array(
+        'Accept' => 'application/json',
+        'Authorization' => $access_token['token_type'] .' '. $access_token['access_token']
+      ),
+      'query' => array(
+        'q' => 'Metallica',
+        'type' => 'album,artist'
+      )
+    );
+
+    $uri = 'https://api.spotify.com/v1/search';
+    $response = \Drupal::httpClient()->get($uri, $options);
+
+    return (string) $response->getBody();
+
+    #return var_dump($access_token);
+    return implode('|', $access_token) .'|'. $date->getTimestamp() .'|'. ($access_token['issued_at'] + 300);
+    #return $access_token->get('access_token');
+
+    #$response = \Drupal::httpClient()->post($uri, $options);
+    $data = $response->getBody()->getContents();
+
+    #$uri = 'http://api.spotify.com/search?q=artist:Metallica';
+    #$response = \Drupal::httpClient()->get($uri, array('headers' => array('Accept' => 'application/json')));
+    #$data = (string) $response->getBody();
+
+    return $data;
+    /*
     if ($client_id !== "" && $client_id) {
       return $client_id;
     }
+    */
 
+
+
+    /*
     $time = new \DateTime();
     if ((int) $time->format('G') >= 00 && (int) $time->format('G') < 12) {
       return $this->t('Good morning world');
@@ -63,6 +157,7 @@ class MusicSearchService {
     if ((int) $time->format('G') >= 18) {
       return $this->t('Good evening world');
     }
+    */
   }
 
 }
